a019479985cb00cbcfcbfbff44fc3bb3b95d6525
Jyotiranjan Sahoo <jsahoo.p@gmail.com>
2024-05-24 23:02:00 +0530

    add config files for using keys

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,17 @@
+import config from 'config';
 import express from 'express';
 import { documentRouter } from './routes/documentRoutes';
 
 const app = express();
-const port = process.env.port || 3000;
+const port = config.get('server.port') as number;
+const host = config.get('server.host') as string;
+
+app.get('/', (_, res) => {
+  res.send('Hello World!');
+});
 
 app.use('/api/v1/documents', documentRouter);
 
 app.listen(port, () => {
-  console.log(`Server listening on port http://localhost:${port}`);
+  console.log(`Server listening on port http://${host}:${port}`);
 });
