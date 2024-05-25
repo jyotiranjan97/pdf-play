27a842ed81ad907c484eaf13eceea5583c17dea3
Jyotiranjan Sahoo <jsahoo.p@gmail.com>
2024-05-25 09:59:21 +0530

    connect to mongodb

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,13 @@
 import config from 'config';
 import express from 'express';
+import mongoose from 'mongoose';
 import { documentRouter } from './routes/documentRoutes';
 
 const app = express();
 const port = config.get('server.port') as number;
 const host = config.get('server.host') as string;
+
+connectToDatabase();
 
 app.get('/', (_, res) => {
   res.send('Hello World!');
@@ -15,3 +18,18 @@ app.use('/api/v1/documents', documentRouter);
 app.listen(port, () => {
   console.log(`Server listening on port http://${host}:${port}`);
 });
+
+// ==============================
+// ***** Utility functions ******
+// ==============================
+
+async function connectToDatabase() {
+  const connectionUrl = config.get('database.url') as string;
+  const dbName = config.get('database.name') as string;
+  try {
+    await mongoose.connect(`${connectionUrl}/${dbName}`);
+    console.log('Connected to database');
+  } catch (error) {
+    console.error('Error connecting to database:', error);
+  }
+}
